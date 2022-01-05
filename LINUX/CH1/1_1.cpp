a4b121e9223790688ca44f31c42bacc274938f9a
ramikoh <kohil.ikram@hotmail.com>
2022-01-05 16:19:44 -0500

    Completed CH1 Q.1

--- LINUX/CH1/1_1.cpp
+++ LINUX/CH1/1_1.cpp
@@ -2,8 +2,32 @@
 #include <string>
 #include <iostream>
 
+bool isUniqueString(std::string inputString) {
+    const int MAX_CHARS_ASCII = 256;
+
+    if(inputString.length() >= MAX_CHARS_ASCII) {
+        return false;
+    }
+
+    bool charList[MAX_CHARS_ASCII] = { false };
+    for (char letter : inputString) {
+        int index = letter - '0';
+
+        if(charList[index]) {
+            return false;
+        }
+
+        charList[index] = true;
+    }
+    
+    return true;
+}
+
 int main() {
     std::string inputString;
-    std::cin << inputString;
+    std::cout << "Enter the string to verify: ";
+    std::cin >> inputString;
 
+    std::cout << std::boolalpha << "String is unique: " << isUniqueString(inputString) << std::endl;
+    
 }
